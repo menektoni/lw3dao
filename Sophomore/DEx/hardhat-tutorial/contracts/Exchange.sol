93e7a58952a958bc958a9ffdf87ce54eb23a7789
Toni Domenech Borrell <tonidome@gmail.com>
2022-05-09 21:55:46 +0200

    Adding some functionalities. CryptoDevTokenToEth remaining.

--- Sophomore/DEx/hardhat-tutorial/contracts/Exchange.sol
+++ Sophomore/DEx/hardhat-tutorial/contracts/Exchange.sol
@@ -35,7 +35,6 @@ contract Exchange is ERC20 {
 
         } else {
 
-            // Concepts here aren't clear. Rewatch.
             uint ethReserve = ethBalance - msg.value;
             uint cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve) / (ethReserve);
 
@@ -43,10 +42,63 @@ contract Exchange is ERC20 {
 
             cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
 
+            // Check where liquidity comes from
             liquidity = (totalSupply() * msg.value)/(ethReserve);
 
             _mint(msg.sender, liquidity);
         }
         return liquidity;
     }
+
+    function remove_liquidity (uint _amount) public returns (uint , uint) {
+        // The mission of this function is to be able for the user to remove liquidity
+        require(_amount > 0, "You don't have LP");
+        uint ethReserve = address(this).balance;
+        uint _totalSupply = totalSupply();
+
+        uint ethAmount = (ethBalance * _amount)/(_totalSuply);
+
+        uint cryptoDevTokenAmount = (getReserve() * _amount)/(_totalSupply);
+
+        // Burn is an openzeppelin built-in function.
+        _burn(msg.sender, _amount);
+        // Transfer `ethAmount` of Eth from user's wallet to the contract
+        payable(msg.sender).transfer(ethAmount);
+        // Transfer `cryptoDevTokenAmount` of Crypto Dev tokens from the user's wallet to the contract
+        ERC20(cryptoDevTokenAddress).transfer(msg.sender, cryptoDevTokenAmount);
+
+        return (ethAmount, cryptoDevTokenAmount);
+    }
+
+    // Let's calculate the amount of tokens
+    function getAmountOfTokens(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) public pure returns (uint256) {
+        require (inputReserve > 0 && outputReserve > 0, "Out of reserves");
+        // Fee charging
+        uint256 inputAmountWithFee = inputAmount * 99;
+
+        uint numerator = inputAmountWithFee * outputReserve;
+        uint denominator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator/denominator;
+    }
+    // Let's make the swap ether for CryptoDevsToken
+    function ethCryptoDevToken(uint _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+
+        uint256 tokensBought = getAmountOfTokens(
+            msg.value, 
+            address(this).balance - msg.value, 
+            tokenReserve); // Why value of outputReserve is tokenReserve
+
+        require (tokensBought >= _minTokens, "Insufficient output amount");
+
+        ERC20(cryptoDevTokenAddress).transfer(msg.sender, tokensBought);
+    } 
+
+    // Let's make the swap CryptoDevToken for ether
+    
 }
