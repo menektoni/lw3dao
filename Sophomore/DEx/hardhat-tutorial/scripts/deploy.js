18dd49065b22aac3dd3a0b20f90d41e317f67f23
Toni Domenech Borrell <tonidome@gmail.com>
2022-05-09 23:39:51 +0200

    Starting the deploy script.

---
+++ Sophomore/DEx/hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,9 @@
+const { ethers } = require("hardhat");
+require("dotenv").congif( {path: ".env" });
+const { CRYPTO_DEV_TOKEN_ADDRESS } = require("../constants");
+
+async function main() {
+    const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_ADDRESS;
+
+    
+}
