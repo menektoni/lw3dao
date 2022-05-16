26677990a82e723f1fab03ab4cfbd6e23c4fb999
Toni Domenech Borrell <tonidome@gmail.com>
2022-05-16 23:57:10 +0200

    Finished the utils scripts.

--- Sophomore/DEx/my-app/utils/swap.js
+++ Sophomore/DEx/my-app/utils/swap.js
@@ -41,5 +41,44 @@ export const getAmountOfTokensReceivedFromSwap = async (
 }
 
 export const swapTokens = async (
-    
-)
+    signer,
+    swapAmountWei,
+    tokenToBeReceivedAfterSwap,
+    ethSelected
+) => {
+    const exchangeContract = new Contract (
+        EXCHANGE_CONTRACT_ADDRESS,
+        EXCHANGE_CONTRACT_ABI,
+        signer
+    );
+    const tokenContract = new Contract (
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+    );
+    let tx;
+
+  // If Eth is selected call the `ethToCryptoDevToken` function else
+  // call the `cryptoDevTokenToEth` function from the contract
+  // As you can see you need to pass the `swapAmount` as a value to the function because
+  // it is the ether we are paying to the contract, instead of a value we are passing to the function
+  if (ethSelected) {
+      tx = await exchanfeContract.ethToCryptoDevToken(
+          tokenToBeReceivedAfterSwap, {
+              value: swapAmountWei,
+          }
+      );
+  } else {
+      tx = await tokenContract.approve(
+          EXCHANGE_CONTRACT_ADDRESS,
+          swapAmountWei.toString()
+      );
+      await tx.wait();
+
+      tx = await exchangeContract.cryptoDevTokenToETH(
+          swapAmountWei,
+          tokenToBeReceivedAfterSwap
+      );
+  }
+  await tx.wait();
+}
